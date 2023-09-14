74df6ad48905a1d9b419ce3f3cbf4344bc898a33
hsl <flt@kakao.com>
2023-09-14 15:52:46 +0900

    handling multiple COM ports, temporary code

--- WGTSConsole/Program.cs
+++ WGTSConsole/Program.cs
@@ -10,8 +10,10 @@ namespace WGTSConsole
     {
         private static SerialPort serial = new SerialPort();
         private static string g_sRecvData = String.Empty;
+        public static bool serial_gps_received = false;
+        public static int serial_port_number = 0;
         public static int counter = 0;
-        public static int counterForceCloseLimit = 5;
+        public static int counterForceCloseLimit = 10;
 
         [StructLayout(LayoutKind.Sequential)]
         public struct SYSTEMTIME
@@ -31,13 +33,55 @@ namespace WGTSConsole
 
         public static void Main(string[] args)
         {
-            serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
+            serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);            
 
+            int counterForceClose = 0;
+
+            while (true)
+            {
+                if (counter >= 5)
+                {
+                    serial.Close();
+                    Environment.Exit(0);
+                }
+
+                Thread.Sleep(1000);
+
+                counterForceClose++;
+
+                if (counterForceClose >= counterForceCloseLimit)
+                {
+                    serial.Close();
+                    Console.WriteLine("Time expired - 시간 초과로 종료합니다.");
+                    Environment.Exit(-2);
+                }
+
+                if (!serial_gps_received)
+                {
+                    serial.Close();
+                    serial_OpenPort();
+                    serial_port_number += 1;
+                }
+            }
+        }
+
+        private static void serial_OpenPort()
+        {
             string[] ports = SerialPort.GetPortNames();
+
+            if (ports.Length < serial_port_number + 1)
+            {
+                Console.WriteLine("GPS port not found - GPS가 연결된 시리얼포트를 찾지 못했습니다.");
+                Thread.Sleep(3000);
+                Environment.Exit(-2);
+            }
+
+            Console.WriteLine("Discovered COM ports : " + ports.Length + ", Trying COM port #" + serial_port_number.ToString() + "...");
+            Console.WriteLine("발견 포트 개수 : " + ports.Length + ", " + serial_port_number.ToString() + "번 포트 시도 중...");
 
             if (ports.Length != 0)
             {
-                serial.PortName = ports[0];
+                serial.PortName = ports[serial_port_number];
                 serial.BaudRate = 9600;
                 serial.StopBits = StopBits.One;
                 serial.Parity = Parity.None;
@@ -54,29 +98,8 @@ namespace WGTSConsole
             }
             else
             {
-                Console.WriteLine("연결된 시리얼포트를 찾지 못했습니다.");
+                Console.WriteLine("No available COM ports - 연결된 시리얼포트를 찾지 못했습니다.");
                 Environment.Exit(-2);
-            }
-
-            int counterForceClose = 0;
-
-            while (true)
-            {
-                if (counter >= 3)
-                {
-                    serial.Close();
-                    Environment.Exit(0);
-                }
-
-                Thread.Sleep(1500);
-
-                counterForceClose++;
-
-                if (counterForceClose >= counterForceCloseLimit)
-                {
-                    Console.WriteLine("시간 초과로 종료합니다.");
-                    Environment.Exit(-2);
-                }
             }
         }
 
@@ -103,6 +126,7 @@ namespace WGTSConsole
 
                 if ((g_sRecvData != string.Empty) && (recvData[0] == "$GPRMC"))
                 {
+                    serial_gps_received = true;
                     string utctime = recvData[1];    // UTC Time hhmmss.sss
                     string status = recvData[2]; // Valid/Invalid
                     string lat = recvData[3];    // ddmm.mmmm
@@ -124,6 +148,13 @@ namespace WGTSConsole
                         Console.WriteLine(utcfull);
 
                         setWindowsTime(dt);
+
+                        Console.WriteLine("Time synchronization success - 시간 동기화 성공");
+
+                        serial.Close();
+                        Thread.Sleep(3000);
+                        Environment.Exit(0);
+                        return;
                     }
 
                     counter++;
@@ -132,7 +163,7 @@ namespace WGTSConsole
             catch (Exception)
             {
                 g_sRecvData = string.Empty;
-                Console.WriteLine("시간 동기화가 정상적으로 이루어지지 않았습니다.");
+                Console.WriteLine("Unable to synchronize time - 시간 동기화가 정상적으로 이루어지지 않았습니다.");
                 Environment.Exit(-1);
             }
         }
